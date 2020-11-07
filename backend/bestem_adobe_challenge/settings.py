95f6135692b215a3f40ca8b5255851edea1bb0f2
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-11-07 16:21:31 +0200

    modified cors settings

--- backend/bestem_adobe_challenge/settings.py
+++ backend/bestem_adobe_challenge/settings.py
@@ -124,11 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-CORS_ALLOWED_ORIGINS = (
-    'http://localhost:4200',
-    'http://localhost:8000',
-)
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    r"^https://localhost:8000/.*",
-]
+CORS_ORIGIN_ALLOW_ALL = True
