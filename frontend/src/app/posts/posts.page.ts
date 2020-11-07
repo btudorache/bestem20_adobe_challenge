76f27767027324a8311ffa6c408256e405c70616
Adrian Munteanu <adimunteanu13@gmail.com>
2020-11-07 14:29:04 +0100

    feat: add get request for posts

--- frontend/src/app/posts/posts.page.ts
+++ frontend/src/app/posts/posts.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-posts',
@@ -8,40 +9,15 @@ import { Post } from './post.model';
   styleUrls: ['./posts.page.scss'],
 })
 export class PostsPage implements OnInit {
-  public posts: Post[] = [
-    {
-        id: "1",
-        title: "Pictura contemporana",
-        description: "Cea mai tare pictura",
-        imageUrl: "https://valenciatong.files.wordpress.com/2017/04/cropped-colorpastel4a.jpg?w=2000&h=1200&crop=1",
-        artist: "Opakaveli",
-        location: "Peste tot si nicaieri",
-        created_at: new Date("2020-11-07T11:26:39.324000Z")
-    },
-    {
-        id: "2",
-        title: "Alta opera de arta",
-        description: "just a simple hvtghgftf",
-        imageUrl: "https://valenciatong.files.wordpress.com/2017/04/cropped-colorpastel4a.jpg?w=2000&h=1200&crop=1",
-        artist: "xX_Adrian_Xx",
-        location: "Berlin",
-        created_at: new Date("2020-11-07T11:27:15.563000Z")
-    },
-    {
-        id: "3",
-        title: "Sculptura lui torcea",
-        description: "cea mai tare",
-        imageUrl: "https://lh6.ggpht.com/HlgucZ0ylJAfZgusynnUwxNIgIp5htNhShF559x3dRXiuy_UdP3UQVLYW6c=s1200",
-        artist: "Torcea",
-        location: "Focsani",
-        created_at: new Date("2020-11-07T11:27:50.458000Z")
-    }
-];
+  public posts: Post[];
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit() {
-    console.log(this.posts);
+    this.postsService.getPosts().subscribe(posts => this.posts = posts);
   }
 
 }
--- frontend/src/app/posts/posts.service.ts
+++ frontend/src/app/posts/posts.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment.prod';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +11,14 @@ export class PostsService {
 
   constructor(private http: HttpClient) { }
 
-  
+  public getPosts() {
+    return this.http
+      .get(environment.backendApi)
+      .pipe(
+        map(resData => {
+          const postArray: Post[] = Object.values(resData);
+          return postArray;
+        })
+      );
+  }
 }
