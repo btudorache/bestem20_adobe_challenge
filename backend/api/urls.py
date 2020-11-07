beba74a2281e2a29765d2d5c068256ff6d3c2f16
Bogdan Tudorache <bogdanmihait10@gmail.com>
2020-11-07 23:00:38 +0200

    added random detail view

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, RandomDetail
 
 
 urlpatterns = [
     path('', PostList.as_view(), name='posts_list'),
+    path('random/', RandomDetail.as_view(), name='random_detail'),
     path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import generics
 from .serializers import PostSerializer
 from .models import Post
 
+import random
+
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -11,3 +13,12 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class RandomDetail(generics.RetrieveAPIView):
+    serializer_class = PostSerializer
+
+    def get_object(self):
+        all_posts = Post.objects.all()
+        obj = random.choice(all_posts)
+        return obj
